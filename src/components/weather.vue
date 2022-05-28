ecad2c439031bddc9794815ac075ea91de4db01c
Viktor-Wahlstrom <viktor@wiff.se>
2022-05-28 13:37:47 +0200

    väder

--- src/components/weather.vue
+++ src/components/weather.vue
@@ -1,16 +1,60 @@
 <template>
   <div class="weather">
-    <h1>Detta är vädret
-    </h1>
+    <h1>Detta är vädret</h1>
+    <div class="weather-box">
+      <div>{{this.showWeather}}</div>
+      <div>{{this.weahterIcon}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'WeatherApi',
-}
-</script>
+import axios from "axios";
 
+export default {
+  name: "App",
+  data() {
+    return {
+      //api_key: "0d1ac2ea3b5b859686a80649d53906c5",
+      showWeather: "",
+      weahterIcon: ""
+    };
+  },
+
+  mounted() {
+    this.fetchWeather();
+    this.fetchIcon();
+  },
+
+  methods: {
+    fetchWeather() {
+      const url =
+        "https://api.openweathermap.org/data/2.5/weather?id=" + 2712414 + "&appid=" + this.api_key;
+
+      axios.get(url)
+        .then((res) => res.data)
+        .then((data) => {
+          data.weather.map((item) => {
+            this.showWeather = item.description;
+            this.weahterIcon = item.icon;
+          });
+        });
+    },
+    fetchIcon() {
+      const Iconurl = 
+        "http://openweathermap.org/img/wn/" + this.weahterIcon + "@2x.png";
+
+        axios.get(Iconurl)
+        .then((res) => res.data)
+        .then((data) => {
+          data.weather.map((item) => {
+            this.weahterIcon = item.icon;
+              });
+        });
+    }
+  },
+};
+</script>
 <style scoped>
 .weather {
   height: 25rem;
